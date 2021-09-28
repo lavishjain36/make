6de805f3449cab4f35bdb5cf9bc28cf810d670e2
Lavish <lavishjain36@gmail.com>
2021-09-28 12:33:19 +0530

    Added prac

--- script.js
+++ script.js
@@ -1,73 +1,22 @@
-let selectedRow = null;
-function onFormSubmit() {
-  var formData = readFormData();
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
 
-  if (selectedRow == null) insertNewRecord(formData);
-  else updateRecord(formData);
-  //To reset the form with empty  space
-  resetForm();
-}
+  filter = input.value.toUpperCase();
 
-function readFormData() {
-  const formData = {};
+  table = document.getElementById("myTable");
 
-  formData["fullname"] = document.getElementById("fullname").value;
-  formData["empCode"] = document.getElementById("empCode").value;
-  formData["salary"] = document.getElementById("salary").value;
-  formData["city"] = document.getElementById("city").value;
-  return formData;
-  //   console.log(formData);
-}
+  tr = table.getElementsByTagName("tr");
 
-function insertNewRecord(data) {
-  let table = document
-    .getElementById("employeeList")
-    .getElementsByTagName("tbody")[0];
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
 
-  //insert row
-  var newRow = table.insertRow(table.length);
-  cell1 = newRow.insertCell(0);
-  cell1.innerHTML = data.fullname;
-  cell2 = newRow.insertCell(1);
-  cell2.innerHTML = data.empCode;
-  cell3 = newRow.insertCell(2);
-  cell3.innerHTML = data.salary;
-  cell4 = newRow.insertCell(3);
-  cell4.innerHTML = data.city;
-  cell4 = newRow.insertCell(4);
-  cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                   <a onClick="onDelete(this)">Delete</a>`;
-}
-
-function resetForm() {
-  document.getElementById("fullname").value = "";
-  document.getElementById("empCode").value = "";
-  document.getElementById("salary").value = "";
-  document.getElementById("city").value = "";
-  selectedRow = null;
-}
-//using this we are bringing the data in form if user click on edit button
-function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("fullname").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("city").value = selectedRow.cells[3].innerHTML;
-}
-
-//Updating data after edit populate your data in form
-
-function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.fullname;
-  selectedRow.cells[1].innerHTML = formData.empCode;
-  selectedRow.cells[2].innerHTML = formData.salary;
-  selectedRow.cells[3].innerHTML = formData.city;
-}
-
-function onDelete(td) {
-  if (confirm("Do you want to Delete this record?")) {
-    row = td.parentElement.parentElement;
-    document.getElementById("employeeList").deleteRow(row.rowIndex);
-    resetForm();
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
   }
 }
