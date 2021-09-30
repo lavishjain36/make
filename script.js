197be212695da76f87d87e7e50673fca83ed1587
Lavish <lavishjain36@gmail.com>
2021-09-30 12:19:55 +0530

    make

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-// 15 Minutes
-function updateText(val) {
-  document.getElementById("textinput").value = val;
+const getMakeup = async () => {
+  try {
+    const url = `http://makeup-api.herokuapp.com/api/v1/products.json/`;
+    const res = await fetch(url);
+    const makeup = await res.json();
+    createMakeUpCard(makeup);
+    // console.log(makeup);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function createMakeUpCard(makeup) {
+  console.log(makeup);
+  const html = makeup
+    .map((user) => {
+      return `
+      <div class="zoom">
+      <div class='makeup '>
+      <p > <img src="${user.image_link}" alt="${user.name}" srcset="" class='img-container'></p>
+      <div class="info">
+      <p class="type"><b>Brand </b>:  ${user.brand}</p>
+      <p class="name"><b>Name </b>:  ${user.name}</p>
+      <p class="number"><b>Price</b> :  ${user.price} $</p>
+      <p><b>Description </b>:  ${user.description}</p>
+      </div>
+      <p><b>Product-link </b>:  <a href="${user.product_link}" target="_blank"> ${user.product_link}</a></p>
+      
+      </div>
+      
+      </div>
+      `;
+    })
+    .join("");
+  console.log(html);
+  document.querySelector("#app").innerHTML = html;
 }
+
+getMakeup();
